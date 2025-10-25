5e1abf73f16bbca4120d1653cc361cf5af996316
brucemack <bruce@mackinnon.com>
2025-10-25 10:21:55 -0400

    Added some DSP tests

---
+++ dsp-test/lpf-1.py
@@ -0,0 +1,48 @@
+import numpy as np
+from scipy.signal import firwin
+import matplotlib.pyplot as plt
+
+def fir_freq_response(coefficients, sampling_rate):
+    # Calculate the frequency response using the DFT (FFT)
+    frequency_response = np.fft.fft(coefficients)    
+    # Number of coefficients
+    num_coeffs = len(coefficients)
+    # Generate the corresponding frequency values
+    frequencies = np.fft.fftfreq(num_coeffs, 1 / sampling_rate)
+    return frequencies, frequency_response
+
+
+sample_rate = 48000
+nyq_rate = sample_rate / 2.0
+# The cutoff frequency of the filter.
+cutoff_hz = 4300
+# The desired width of the transition from pass to stop,
+# relative to the Nyquist rate. 
+#width = (6300 - 2900) / nyq_rate
+# The desired ripple in dB
+#ripple_db = 0.5
+
+# Create a FIR filter using a methodology
+N = 31
+beta = 3.0
+# Use firwin with a Kaiser window to create a lowpass FIR filter.
+taps = firwin(N, cutoff_hz / nyq_rate, window=('kaiser', beta)) * 32767
+
+# The FIR filter from the ASL code
+N2 = 31
+taps2 = [ 103, 136, 148, 74, -113, -395, -694,
+        -881, -801, -331, 573, 1836, 3265, 4589, 5525, 5864, 5525,
+        4589, 3265, 1836, 573, -331, -801, -881, -694, -395, -113,
+        74, 148, 136, 103 ]
+
+# Calculate the frequency response
+frequencies, frequency_response = fir_freq_response(taps, sample_rate)
+db_data = 20 * np.log10(np.abs(frequency_response[:len(frequencies)//2]))
+
+fig, ax = plt.subplots()
+ax.set_title('LPF Filter Impulse Response')
+ax.plot(taps, label="fc=4.3kHz, Kaiser beta=3.0")
+ax.plot(taps2,label="chan_simpleusb.c")
+ax.grid(True)
+ax.legend()
+plt.show()
